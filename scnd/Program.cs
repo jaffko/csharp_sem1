876398e28b4815d3f4602b0d0e9b3ed2042011ea
Ildar <steel14@yandex.ru>
2022-08-31 22:00:39 +0500

    added scnd program

---
+++ scnd/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите два числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a)
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
