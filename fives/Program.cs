dcee316b50f3e3c3801e1e2c85698fd18fd20964
Ildar <steel14@yandex.ru>
2022-08-31 23:25:06 +0500

    added last number in 3-digit number

---
+++ fives/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 3-х значное число");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(a % 10);
+}
+catch
+{
+    
+    Console.WriteLine("Нужно число");
+}
