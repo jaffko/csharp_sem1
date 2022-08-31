2c1f9e46fc6641b4b3fa87fa110a5cdac8b50ada
Ildar <steel14@yandex.ru>
2022-08-31 21:50:45 +0500

    first program

---
+++ first/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a * a;
+Console.WriteLine("Квадрат числа равен " + b);
+if (a>0)
+{
+    Console.WriteLine("Число положительное");
+}
+else if (a<0) Console.WriteLine("Число отрицательное");
+else Console.WriteLine("Число равно нулю");
