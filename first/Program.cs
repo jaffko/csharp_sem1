22cdb7e6dc43c573be7998f69b62778dc1712463
Ildar <steel14@yandex.ru>
2022-08-31 22:26:45 +0500

    added try catch in fist task

--- first/Program.cs
+++ first/Program.cs
@@ -1,10 +1,18 @@
 ﻿Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = a * a;
-Console.WriteLine("Квадрат числа равен " + b);
-if (a>0)
+try
 {
-    Console.WriteLine("Число положительное");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = a * a;
+    Console.WriteLine("Квадрат числа равен " + b);
+    if (a>0)
+    {
+        Console.WriteLine("Число положительное");
+    }
+    else if (a<0) Console.WriteLine("Число отрицательное");
+    else Console.WriteLine("Число равно нулю");
 }
-else if (a<0) Console.WriteLine("Число отрицательное");
-else Console.WriteLine("Число равно нулю");
+catch 
+{
+    
+   Console.WriteLine("Нужно целое число!"); 
+}
